3919b4b2dac2535d102189f5a0bf6387aac1a1ae
benkorsan <103997850+benkorsan@users.noreply.github.com>
2022-08-20 12:08:01 +0300

    Update rmtrash.py

--- program/rmtrash.py
+++ program/rmtrash.py
@@ -17,9 +17,9 @@ async def clear_downloads(_, message: Message):
     if ls_dir:
         for file in os.listdir(downloads):
             os.remove(os.path.join(downloads, file))
-        await message.reply_text("✅ **deleted all downloaded files**")
+        await message.reply_text("✅ **indirilen tüm dosyaları sildi**")
     else:
-        await message.reply_text("❌ **no files downloaded**")
+        await message.reply_text("❌ **dosya indirilmedi**")
 
         
 @Client.on_message(command(["rmw", "clean"]) & ~filters.edited)
@@ -31,9 +31,9 @@ async def clear_raw(_, message: Message):
         for file in os.listdir(raw):
             if file.endswith('.raw'):
                 os.remove(os.path.join(raw, file))
-        await message.reply_text("✅ **deleted all raw files**")
+        await message.reply_text("✅ **tüm ham dosyaları sildi**")
     else:
-        await message.reply_text("❌ **no raw files found**")
+        await message.reply_text("❌ **ham dosya bulunamadı**")
 
 
 @Client.on_message(command(["cleanup"]) & ~filters.edited)
@@ -45,6 +45,6 @@ async def cleanup(_, message: Message):
     if ls_dir:
         for dta in os.listdir(pth):
             os.system("rm -rf *.raw *.jpg")
-        await message.reply_text("✅ **cleaned**")
+        await message.reply_text("✅ **Temizlenmiş**")
     else:
-        await message.reply_text("✅ **already cleaned**")
+        await message.reply_text("✅ **zaten temizlenmiş**")

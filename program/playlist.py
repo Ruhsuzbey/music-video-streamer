97171b25e69e0a2e855eadef3953ec23c7146480
benkorsan <103997850+benkorsan@users.noreply.github.com>
2022-08-20 11:34:05 +0300

    Update playlist.py

--- program/playlist.py
+++ program/playlist.py
@@ -15,7 +15,7 @@ from driver.filters import command, other_filters
 
 
 keyboard = InlineKeyboardMarkup(
-    [[InlineKeyboardButton("🗑 Close", callback_data="cls")]]
+    [[InlineKeyboardButton("🗑 Kapatmak", callback_data="cls")]]
 )
 
 
@@ -25,9 +25,9 @@ async def playlist(client, m: Message):
    if chat_id in QUEUE:
       chat_queue = get_queue(chat_id)
       if len(chat_queue)==1:
-         await m.reply(f"💡 **Currently Streaming:**\n\n• [{chat_queue[0][0]}]({chat_queue[0][2]}) | `{chat_queue[0][3]}`", reply_markup=keyboard, disable_web_page_preview=True)
+         await m.reply(f"💡 **Şu anda Akış:**\n\n• [{chat_queue[0][0]}]({chat_queue[0][2]}) | `{chat_queue[0][3]}`", reply_markup=keyboard, disable_web_page_preview=True)
       else:
-         QUE = f"💡 **Currently Streaming:**\n\n• [{chat_queue[0][0]}]({chat_queue[0][2]}) | `{chat_queue[0][3]}` \n\n**📖 Queue List:**\n"
+         QUE = f"💡 **Şu anda Akış:**\n\n• [{chat_queue[0][0]}]({chat_queue[0][2]}) | `{chat_queue[0][3]}` \n\n**📖 Kuyruk Listesi:**\n"
          l = len(chat_queue)
          for x in range (1, l):
             han = chat_queue[x][0]
@@ -36,4 +36,4 @@ async def playlist(client, m: Message):
             QUE = QUE + "\n" + f"**#{x}** - [{han}]({hok}) | `{hap}`"
          await m.reply(QUE, reply_markup=keyboard, disable_web_page_preview=True)
    else:
-      await m.reply("❌ **nothing is currently streaming.**")
+      await m.reply("❌ **şu anda hiçbir şey yayınlanmıyor.**")
